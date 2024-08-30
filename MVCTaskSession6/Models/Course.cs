6da0a0cbe71407eda10bf0f3a9bac4f5c65df4be
Hisham Salah <Hisham Salah@DESKTOP-BCIHSE4>
2024-08-30 04:22:14 +0300

    Add project files.

---
+++ MVCTaskSession6/Models/Course.cs
@@ -0,0 +1,12 @@
+﻿namespace MVC_Task2.Models
+{
+	public class Course
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public float Degree { get; set; }
+		public float MinDegree { get; set; }
+		public virtual ICollection <CourseResult> CourseResults  { get; set; }
+		public virtual ICollection<Instructor> Instructors { get; set; }
+	}
+}
---
+++ MVCTaskSession6/Models/CourseResult.cs
@@ -0,0 +1,13 @@
+﻿namespace MVC_Task2.Models
+{
+	public class CourseResult
+	{
+		public int Id { get; set; }
+		public float Degree { get; set; }
+		public int TraineeId { get; set; }
+		public int CourseId { get; set; }
+		public virtual Trainee Trainee { get; set; }
+		public virtual Course Course { get; set; }
+
+	}
+}
